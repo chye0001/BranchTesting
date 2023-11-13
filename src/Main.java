c2c39aab1d87ce233ec6cfb61f7b66ead2ffda61
chye0001 <chye0001@stud.kea.dk>
2023-11-13 14:20:49 +0100

    Added: testcode of showMood-method in main

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,15 @@ public class Main {
         Dog dog = new Dog("Labrador");
         Cat cat = new Cat("Hellig birma");
 
-        System.out.println(dog.makeSound());
-        System.out.println(cat.makeSound());
+        System.out.println("dog says: " + dog.makeSound());
+        System.out.println("cat says: " + cat.makeSound());
 
-        System.out.println("\n" + cat.eat("fish"));
-        System.out.println(dog.eat("steak"));
+        System.out.println("\n" + "Does cat like fish: " + cat.eat("fish"));
+        System.out.println("Does dog like steak: " + dog.eat("steak"));
+
+        System.out.println("\n" + dog.showMood(true));
+        System.out.println(dog.showMood(false));
+        System.out.println(cat.showMood(true));
+        System.out.println(cat.showMood(false));
     }
 }
